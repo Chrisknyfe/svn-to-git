ce1e6cd17efea9f1d1f3d8bd6ed3fd5928b392ef
Zach Bernal <zbernal@vitalconnect.com>
2014-07-31 10:25:02 -0700

    Removed comments in git-filter-prefix

--- git-filter-prefix.sh
+++ git-filter-prefix.sh
@@ -3,15 +3,6 @@
 PREFIX=$1
 echo Filtering `pwd` as if it were developed inside \"$PREFIX\"
 
-#git filter-branch -f --tree-filter '
-#TEMP=iCouldntGetTheOfficialScriptToWork
-#if [[ ! -e '"$PREFIX"' ]]; then
-#	mkdir -p $TEMP
-#    git ls-tree --name-only $GIT_COMMIT | xargs -I files mv files $TEMP
-#    mkdir -p '"$PREFIX"'
-#    mv $TEMP/* '"$PREFIX"'/
-#fi'
-
 git filter-branch -f --index-filter \
 	'git ls-files -s | sed "s-	\"*-&'"$PREFIX"'/-" > .gitlsfilesprefix
 	GIT_INDEX_FILE_OLD=$GIT_INDEX_FILE
