d848b3e740f946080acb424118da6757820e1108
Zach Bernal <zbernal@vitalconnect.com>
2014-07-30 17:59:22 -0700

    More implementation of create-libraries.

--- create-libraries.py
+++ create-libraries.py
@@ -17,56 +17,74 @@ def readcall(cmd):
     
     
 rootrepo = "file:///home/chrisknyfe/migration/localsvn"
-    
+
 libraries= {
-            "common":                   rootrepo + "/trunk/src/sensory/Libs/common/dev/common/ common_exp",
-            "dsdef_pkt":                rootrepo + "/trunk/src/sensory/Libs/dsdef_pkt/ dsdef_pkt_exp",
-            "biosensor_algorithms":     rootrepo + "/trunk/src/sensory/Libs/biosensor_algorithms/dev/implementation/ biosensor_algorithms_exp",
-            "openssl":                  rootrepo + "/trunk/src/sensory/Libs/external/openssl/ openssl_exp",
-            "yajl":                     rootrepo + "/trunk/src/sensory/Libs/external/yajl/ yajl_exp",
-            "fw_bundle":                rootrepo + "/trunk/src/sensory/Libs/fw_bundle/dev/fw_bundle/ fw_bundle_exp",
-            "rslib":                    rootrepo + "/trunk/src/sensory/Libs/rslib/dev/rslib/ rslib_exp",
-            "sensor_stream":            rootrepo + "/trunk/src/sensory/Libs/sensor_stream/dev/sensor_stream/ sensor_stream_exp",
-            "ss_pkt_gen":               rootrepo + "/trunk/src/sensory/Libs/ss_pkt_gen/dev/ss_pkt_gen/ ss_pkt_gen_exp",
-            "store_forward":            rootrepo + "/svn/trunk/src/sensory/Libs/store_forward/dev/ store_forward_exp",
-            "vc_auth":                  rootrepo + "/svn/trunk/src/sensory/Libs/vc_auth/dev/vc_auth/ vc_auth_exp",
-            "vc_cfg":                   rootrepo + "/svn/trunk/src/sensory/Libs/vc_cfg/dev/ vc_cfg_exp",
-            "vc_cmd":                   rootrepo + "/svn/trunk/src/sensory/Libs/vc_cmd/dev/vc_cmd/ vc_cmd_exp",
-            "vc_crypto":                rootrepo + "/svn/trunk/src/sensory/Libs/vc_crypto/dev/vc_crypto/ vc_crypto_exp",
-            "vc_decomp":                rootrepo + "/svn/trunk/src/sensory/Libs/vc_decomp/ vc_decomp_exp",
-            "vc_relay_core":            rootrepo + "/svn/trunk/src/sensory/Libs/vc_relay_core/ vc_relay_core_exp",
-            "vc_sleep":                 rootrepo + "/svn/trunk/src/sensory/Libs/vc_sleep/ vc_sleep_exp",
-            "vc_slp":                   rootrepo + "/svn/trunk/src/sensory/Libs/vc_slp/dev/vc_slp/ vc_slp_exp",
-            "vc_timer":                 rootrepo + "/svn/trunk/src/sensory/Libs/vc_timer/dev/vc_timer/ vc_timer_exp",
-            "vcpp":                     rootrepo + "/svn/trunk/src/sensory/Libs/vcpp/dev/vcpp/ vcpp_exp",
-            "vcsensorstoolbox":         rootrepo + "/svn/trunk/src/sensory/Tools/VCSensorsToolbox/ vcsensorstoolbox_exp",
+            "common":                   rootrepo + "/trunk/src/sensory/Libs/common/dev/common/",
+            "dsdef_pkt":                rootrepo + "/trunk/src/sensory/Libs/dsdef_pkt/",
+            "biosensor_algorithms":     rootrepo + "/trunk/src/sensory/Libs/biosensor_algorithms/dev/implementation/",
+            "openssl":                  rootrepo + "/trunk/src/sensory/Libs/external/openssl/",
+            "yajl":                     rootrepo + "/trunk/src/sensory/Libs/external/yajl/",
+            "fw_bundle":                rootrepo + "/trunk/src/sensory/Libs/fw_bundle/dev/fw_bundle/",
+            "rslib":                    rootrepo + "/trunk/src/sensory/Libs/rslib/dev/rslib/",
+            "sensor_stream":            rootrepo + "/trunk/src/sensory/Libs/sensor_stream/dev/sensor_stream/",
+            "ss_pkt_gen":               rootrepo + "/trunk/src/sensory/Libs/ss_pkt_gen/dev/ss_pkt_gen/",
+            "store_forward":            rootrepo + "/trunk/src/sensory/Libs/store_forward/dev/",
+            "vc_auth":                  rootrepo + "/trunk/src/sensory/Libs/vc_auth/dev/vc_auth/",
+            "vc_cfg":                   rootrepo + "/trunk/src/sensory/Libs/vc_cfg/dev/",
+            "vc_cmd":                   rootrepo + "/trunk/src/sensory/Libs/vc_cmd/dev/vc_cmd/",
+            "vc_crypto":                rootrepo + "/trunk/src/sensory/Libs/vc_crypto/dev/vc_crypto/",
+            "vc_decomp":                rootrepo + "/trunk/src/sensory/Libs/vc_decomp/",
+            "vc_relay_core":            rootrepo + "/trunk/src/sensory/Libs/vc_relay_core/",
+            "vc_sleep":                 rootrepo + "/trunk/src/sensory/Libs/vc_sleep/",
+            "vc_slp":                   rootrepo + "/trunk/src/sensory/Libs/vc_slp/dev/vc_slp/",
+            "vc_timer":                 rootrepo + "/trunk/src/sensory/Libs/vc_timer/dev/vc_timer/",
+            "vcpp":                     rootrepo + "/trunk/src/sensory/Libs/vcpp/dev/vcpp/",
+            "vcsensorstoolbox":         rootrepo + "/trunk/src/sensory/Tools/VCSensorsToolbox/",
             }
-            
+              
 gitfilterprefix = os.path.abspath('git-filter-prefix.sh')
 gitcherrysort = os.path.abspath('git-cherry-sort.sh')
+gitusers = os.path.abspath('gitusers.txt')
 
+# Make a subfolder to contain all these wild shenanigans
 if not os.path.exists('libraries_build'):
     os.mkdir('libraries_build')
 os.chdir('libraries_build')
 rootleveldir = os.getcwd()
 
-print libraries
-print gitfilterprefix
-print gitcherrysort
-print rootleveldir
+# Make a "libraries" git repo that we'll fill with these libraries
 
-exit()
+librariesrepo = os.path.abspath('libraries')
+
+if not os.path.exists(librariesrepo):
+    os.mkdir(librariesrepo)
+os.chdir(librariesrepo)
+call("git init")
+with open(".gitignore", 'a') as gitignore:
+    gitignore.write("**/.svn/**\n")
+call("git add .gitignore")
+call("git commit -m 'Create libraries repo'")
+
 for modname, url in libraries.iteritems():
-    dirname = "cloned_" + modname
-    retval = call("git svn clone -A gitusers.txt %s %s" % (url, dirname))
+    os.chdir(rootleveldir)
+    dirname = os.path.abspath("cloned_" + modname)
+    retval = call("git svn clone -A %s %s %s" % (gitusers, url, dirname))
     if retval != 0:
-        raise RuntimeError("git-svn error")
+        raise RuntimeError("git svn clone error")
         
     os.chdir(dirname)
     retval = call("%s %s" % (gitfilterprefix, modname))
     if retval != 0:
         raise RuntimeError("git-filter-prefix error")
-    os.chdir(rootleveldir)
-    exit()
+        
+    os.chdir(librariesrepo)
+    retval = call("git pull --no-edit %s" % (dirname))
+    if retval != 0:
+        raise RuntimeError("git pull error")
+    
+os.chdir(librariesrepo)
+retval = call(gitcherrysort)
+if retval != 0:
+    raise RuntimeError("git-cherry-sort error")
         
         
--- git-filter-prefix.sh
+++ git-filter-prefix.sh
@@ -18,6 +18,6 @@ git filter-branch -f --index-filter \
 	GIT_INDEX_FILE=$GIT_INDEX_FILE.new
 	git update-index --index-info < .gitlsfilesprefix
 	GIT_INDEX_FILE=$GIT_INDEX_FILE_OLD
-	if [[ -e "$GIT_INDEX_FILE.new" ]]; then
+	if [ -e "$GIT_INDEX_FILE.new" ]; then
 		mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"
 	fi' HEAD
